ca16ee2e41a2946202bd20e663caf19678e5a679
SURAJ0866 <106803883+SURAJ0866@users.noreply.github.com>
2023-04-25 02:04:09 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,26 @@
+import Asign from "./component/Asign";
+import salm from "./picture/salm.png"
+function App() {
+return(
+  <>
+<Asign name="hritik rosan"/><br></br>
+<Asign name="sahid kapoor"/><br></br>
+<Asign name="salman khan"
+      imgae={{salm}} />
+ 
+  
+
+</>
+
+
+
+
+)
+}
+
+
+
+
+
+
+export default App;
